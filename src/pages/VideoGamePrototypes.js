3e0ab480dddbc855d914461e549856c115a3342f
jwdavis1989 <jwdavis8589@gmail.com>
2023-04-16 13:27:34 -0500

    Hosting ArcadeMechSmupPrototype

--- src/pages/VideoGamePrototypes.js
+++ src/pages/VideoGamePrototypes.js
@@ -1,11 +1,25 @@
 import GamesSharpIcon from '@mui/icons-material/GamesSharp';
+import DownloadIcon from '@mui/icons-material/Download';
+import { Grid } from '@mui/material';
+import MechShmupLink from "../resources/gamePrototypes/MechSchmup/ArcadeMechShootEmUpVersion01.exe";
 
 function VideoGamePrototypes() {
     return (
         <div className="NormalPageLayout">
             <p className="HeaderTitleNoHover">
-            <GamesSharpIcon fontSize='inherit'/> VIDEO GAME PROTOTYPES
+                <GamesSharpIcon fontSize='inherit' /> VIDEO GAME PROTOTYPES
             </p>
+            <h1 className="textFontPixel">
+                ARCADE TRANSFORMING MECH SHOOT-EM-UP
+            </h1>
+            <Grid container spacing={1}>
+            <Grid item xs={4}/>
+                <Grid item xs={4}>
+                    <a href={MechShmupLink} className="HeaderTitle" target="_blank" rel="noreferrer">
+                        <DownloadIcon fontSize='inherit' /> DOWNLOAD INSTALLER
+                    </a>
+                </Grid>
+            </Grid>
         </div>
     );
 }
