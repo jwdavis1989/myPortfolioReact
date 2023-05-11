b118219d06103cd215fbef9230d74b53321c833b
jwdavis1989 <jwdavis8589@gmail.com>
2023-05-11 15:52:26 -0500

    Significant Update to Phoenix Walker Page

---
+++ src/components/gamePrototypeTemplates/PhoenixBombTemplate.js
@@ -0,0 +1,30 @@
+
+function PhoenixBombTemplate() {
+    return (
+        <div className="NormalPageLayoutLeft ScrollingBox">
+            <p>
+                Screen-clearing bombs are a common sight in many shmups, however Phoenix Missiles are much more fun.
+                <br />Instead of a simple click followed by a boom killing all enemies, Phoenix Missiles instead freeze time so the 
+                player can strategize and express skillful planning.
+                <br />(<b>Tip:</b> Corvette enemies have a 100% drop chance for Phoenix Missiles. Use this to your advantage in the boss fight!)
+                <p /><b>How To Use:</b>
+                <ol>
+                    <li>
+                        Pick up the glowing red bombs with a P on them to gain a Phoenix Missile.
+                    </li>
+                    <li>
+                        Right click to freeze time.
+                    </li>
+                    <li>
+                        Left click to set each waypoint you want the missile to fly through.
+                    </li>
+                    <li>
+                        After picking the final waypoint, the Phoenix Missile launches and destroys enemies within its path!
+                    </li>
+                </ol>
+            </p>
+        </div>
+    )
+}
+
+export { PhoenixBombTemplate };
--- src/pages/VideoGamePrototypes.js
+++ src/pages/VideoGamePrototypes.js
@@ -1,10 +1,19 @@
 import GamesSharpIcon from '@mui/icons-material/GamesSharp';
 import DownloadIcon from '@mui/icons-material/Download';
 import { Grid } from '@mui/material';
-import MechShmupLink from "../resources/gamePrototypes/MechSchmup/ArcadeMechShootEmUpVersion03.exe";
+import React, { useState } from 'react';
+import MechShmupLink from "../resources/gamePrototypes/MechSchmup/ArcadeMechShootEmUpVersion04.exe";
 import PhoenixWalkerTitleScreen from "../resources/images/gamePrototypes/gifs/PhoenixWalkerTitleScreen/PhoenixWalkerTitleScreen.gif";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+
+import { TwoFormsTwoGenresTemplate } from "../components/gamePrototypeTemplates/TwoFormsTwoGenresTemplate";
+import { PhoenixBombTemplate } from "../components/gamePrototypeTemplates/PhoenixBombTemplate";
 
 function VideoGamePrototypes() {
+    const [hookOneExpanded, setHookOneExpanded] = useState(0);
+    const [hookTwoExpanded, setHookTwoExpanded] = useState(0);
+
     return (
         <div className="NormalPageLayout">
             <p className="HeaderTitleNoHover">
@@ -13,18 +22,56 @@ function VideoGamePrototypes() {
             <h1 className="textFontPixel">
                 PHOENIX WALKER
             </h1>
-            <Grid container spacing={1}>
-            <Grid item xs={4}/>
+            <Grid container spacing={0}>
                 <Grid item xs={4}>
-                    <img src={PhoenixWalkerTitleScreen} width="100%" alt='Phoenix Walker' className="SWWBanner"></img>
-                </Grid>
-            </Grid>
-            <Grid container spacing={1}>
-            <Grid item xs={4}/>
-                <Grid item xs={4}>
-                    <a href={MechShmupLink} className="HeaderTitle" target="_blank" rel="noreferrer">
+                    <img src={PhoenixWalkerTitleScreen} width="100%" alt='Phoenix Walker' className="SWWBanner" />
+                    <p /><a href={MechShmupLink} className="HeaderTitle" target="_blank" rel="noreferrer">
                         <DownloadIcon fontSize='inherit' /> DOWNLOAD INSTALLER
                     </a>
+                </Grid>
+                <Grid item xs={4}>
+                    <p className="SmallHeaderTitleNoHover width90">
+                        <b>SUMMARY:</b>
+                    </p>
+                    <div className="NormalPageLayoutLeft moveLeft5 ScrollingBox">
+                        Have you ever played a shmup ("shoot-em-up") game and thought to yourself, "What if I could put this thing in reverse and
+                        check out a different direction or go back for something I missed?"
+                        <br />Do you ever find yourself thinking, "Yeah, this game is pretty cool, but I wish I could transform into a giant robot mech?"
+                        <br />It's okay, we've all been there before my friend.
+                        <br />Phoenix Walker is a fun genre-bending game that lets you freely switch between an on-the-rails shmup and an omni-directional
+                        shooter similar to a twin-stick shooter.
+                        <br />Can you master both forms to beat the boss?
+                    </div>
+                </Grid>
+                <Grid item xs={4}>
+                    <p className="SmallHeaderTitleNoHoverEvenMargin width100">
+                        <b>WHAT'S THE HOOK?</b>
+                    </p>
+                    <div className="NormalPageLayout moveLeft15 width100">
+                        <p>Click a Button to Expand the Section.</p>
+                        {hookOneExpanded ?
+                            <div>
+                                <div className="AccordionButton width90" onClick={() => setHookOneExpanded(!hookOneExpanded)}>
+                                    Two Forms-Two Genres <ExpandLessIcon fontSize='inherit' />
+                                </div>
+                                <TwoFormsTwoGenresTemplate />
+                            </div>
+                            : <div className="AccordionButton" onClick={() => setHookOneExpanded(!hookOneExpanded)}>
+                                Two Forms-Two Genres <ExpandMoreIcon fontSize='inherit' />
+                            </div>
+                        }
+                        {hookTwoExpanded ?
+                            <div>
+                                <div className="AccordionButton textAlignLeft" onClick={() => setHookTwoExpanded(!hookTwoExpanded)}>
+                                Custom Phoenix Missile Path <ExpandLessIcon fontSize='inherit' />
+                                </div>
+                                <PhoenixBombTemplate />
+                            </div>
+                            : <div className="AccordionButton textAlignLeft" onClick={() => setHookTwoExpanded(!hookTwoExpanded)}>
+                                Custom Phoenix Missile Path <ExpandMoreIcon fontSize='inherit' />
+                            </div>
+                        }
+                    </div>
                 </Grid>
             </Grid>
         </div>
