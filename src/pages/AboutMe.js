d26ef104f2fa4a8feca2347eb3e78653177f6f21
jwdavis1989 <jwdavis8589@gmail.com>
2023-05-22 12:02:44 -0500

    Added the Games I love and Why Section to About Me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -11,9 +11,9 @@ function AboutMe() {
             </p>
             <div className="NormalPageLayout">
                 <b>Bachelors of Computer Science with a Minor in Business</b>
-                <br/>
+                <br />
                 Arkansas A-State University, Jonesboro AR
-                <br/>
+                <br />
                 <i>08/2016 – 05/2020</i>
             </div>
             <br />
@@ -46,7 +46,31 @@ function AboutMe() {
                 <SportsEsportsIcon fontSize='inherit' /> GAMES I LOVE AND WHY
             </p>
             <div className="NormalPageLayoutLeft">
-                Coming soon to a website near you!
+                I love games that:
+                <br />
+                <ul>
+                    <li>Aren't afraid to let the player think and figure it out without "God of War: Ragnarok-syndrome,"
+                        where a character blurts the solution either immediately or if the player takes more than 2 seconds to solve a puzzle.
+                        I always enjoy the "Ah-ha!" moment when you finally solve the puzzle.
+                        <br />&emsp; (e.g. Dark Souls, Outer Wilds, We Were Here Together, Eldin Ring, Zelda)
+                    </li>
+                    <li>Present players with compelling choices with impactful outcomes.
+                        <br />&emsp; (e.g. XCOM, Sid Meier's Civilization, Age of Wonders, Mass Effect, Dragon Age)
+                    </li>
+                    <li>Reward and promote creativity and innovation.
+                        <br />&emsp; (e.g. Outer Wilds, Zelda: Tears of The Kingdom, Banjo-Kazooie: Nuts & Bolts, Dark Cloud 2)
+                    </li>
+                    <li>Allow for specialization & reliance on teammates in multiplayer games.
+                    <br />&emsp; (e.g. World of Warcraft, Final Fantasy 14, Risk of Rain, Deep Rock Galactic)
+                    </li>
+                    <li>Have strong thematic atmospheres.
+                    <br />&emsp; (e.g. Dark Souls, Outer Wilds, Eldin Ring, Armored Core, Bloodbourne, Sekiro: Shadows Die Twice, 
+                    Mass Effect, Dragon Age, Undertale)
+                    </li>
+                    <li>Challenge the player to master their mechanics.
+                    <br />&emsp; (e.g. Dark Souls, Eldin Ring, Armored Core, Risk of Rain)
+                    </li>
+                </ul>
             </div>
             <br />
         </div>
