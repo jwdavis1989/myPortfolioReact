77309ca676597064d20741ec5c63e5f6a7efa983
jwdavis1989 <jwdavis8589@gmail.com>
2023-07-19 20:33:47 -0500

    Removed Unused Dependency.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,5 @@
 import ArticleSharpIcon from '@mui/icons-material/ArticleSharp';
 import DownloadIcon from '@mui/icons-material/Download';
-import { Grid } from '@mui/material';
 import gameDesignPDF from "../resources/handouts/JerryDavisGameDevResume2023.pdf";
 import softwareEngineerPDF from "../resources/handouts/JerryDavisFrontEndResume2023.pdf";
 
@@ -10,13 +9,13 @@ function Resume() {
             <p className="HeaderTitleNoHover">
                 <ArticleSharpIcon fontSize='inherit' /> RESUMES
             </p><br />
-                    <a href={softwareEngineerPDF} className="HeaderTitle" target="_blank" rel="noreferrer">
-                        <DownloadIcon fontSize='inherit' /> Software Engineering
-                    </a><p /><br />
-                    <a href={gameDesignPDF} className="HeaderTitle" target="_blank" rel="noreferrer">
-                        <DownloadIcon fontSize='inherit' /> Game Development
-                    </a>
-            
+            <a href={softwareEngineerPDF} className="HeaderTitle" target="_blank" rel="noreferrer">
+                <DownloadIcon fontSize='inherit' /> Software Engineering
+            </a><p /><br />
+            <a href={gameDesignPDF} className="HeaderTitle" target="_blank" rel="noreferrer">
+                <DownloadIcon fontSize='inherit' /> Game Development
+            </a>
+
             <br />
         </div >
     );
