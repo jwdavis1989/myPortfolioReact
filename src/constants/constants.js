0bb055bb153a56f6fd33e4d4f6cf17f63c31101c
jwdavis1989 <jwdavis8589@gmail.com>
2024-01-03 17:10:49 -0600

    Fixed Button Icon Overflow on Mobile.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,3 +2,4 @@ export const BUTTON_FONT_SIZE = "14px";
 export const BUTTON_ICON_SIZE = "24px";
 export const NAV_BUTTON_FONT_SIZE = "12px";
 export const NAV_BUTTON_ICON_SIZE = "16px";
+export const BUTTON_ICON_MARGIN_RIGHT = "-6px"
